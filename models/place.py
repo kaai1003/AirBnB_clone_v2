66d98fb3223907b8378405b058893056c4cb4618
AIMAD KACEM <kacem.aimad@gmail.com>
2024-04-20 15:08:50 +0100

    update review class to sqlalchemy

--- models/place.py
+++ models/place.py
@@ -2,18 +2,35 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+
+    city_id = Column("city_id", String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column("user_id", String(60), ForeignKey("users.id"), nullable=False)
+    name = Column("name", String(128), nullable=False)
+    description = Column("description", String(1024))
+    number_rooms = Column("number_rooms", Integer, nullable=False, default=0)
+    number_bathrooms = Column("number_bathrooms", Integer, nullable=False, default=0)
+    max_guest = Column("max_guest", Integer, nullable=False, default=0)
+    price_by_night = Column("price_by_night", Integer, nullable=False, default=0)
+    latitude = Column("latitude", Float)
+    longitude = Column("longitude", Float)
+    reviews = relationship("Review", backref="place", cascade="delete")
     amenity_ids = []
+
+    if os.getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """retrieve reviews associated with place"""
+            from models.review import Review
+            from models import storage
+            reviews_list = []
+            all_reviews = storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
--- models/review.py
+++ models/review.py
@@ -7,8 +7,10 @@ from models.place import Place
 from models.user import User
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+
+    place_id = Column("place_id", String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column("user_id", String(60), ForeignKey("users.id"), nullable=False)
+    text = Column("text", String(1024), nullable=False)
